92a27317f153054430e0ab9e78b4665467dd132f
Samuraiii <57580886+Sxmurai@users.noreply.github.com>
2020-05-17 00:55:01 -0500

    delete so new source

--- src/library/managers/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Queue } from "./Queue";
-export { default as Rest } from "./Rest";
--- src/library/structures/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as APIManager } from "./APIManager";
-export { default as API } from "./API";
-export { default as Util } from "./Util";
-export * from "./Configuration";
